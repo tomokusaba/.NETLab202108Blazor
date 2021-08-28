8481e6a1ea605ec67a3fe235b92eba0b46c3a98c
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2021-08-28 16:26:14 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication4/Common/DBAccess.cs
@@ -0,0 +1,45 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Data;
+
+namespace WebApplication4.Common
+{
+    public class DBAccess
+    {
+        public List<TableData> SelectTable(string param)
+        {
+            string sql = "select * from yubin where todofuken || CITY || TYO || YUBIN_7 like :kensaku";
+            List<TableData> list = new List<TableData>();
+            using (DBCommon db = new DBCommon())
+            {
+                using (OracleCommand command = new OracleCommand(sql, db.Con))
+                {
+                    command.Parameters.Add(new OracleParameter("kensaku", $"%{param}%"));
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TableData table = new TableData();
+                            table.Kokyo_cd = DBCommon.GetString(reader, "KOKYO_CD");
+                            table.Yubin_5 = DBCommon.GetString(reader, "YUBIN_5");
+                            table.Yubin_7 = DBCommon.GetString(reader, "YUBIN_7");
+                            table.Todofuke_kana = DBCommon.GetString(reader, "TODOFUKEN_KANA");
+                            table.City_kana = DBCommon.GetString(reader, "CITY_KANA");
+                            table.Tyo_kana = DBCommon.GetString(reader, "TYO_KANA");
+                            table.Todofuken = DBCommon.GetString(reader, "TODOFUKEN");
+                            table.City = DBCommon.GetString(reader, "CITY");
+                            table.Tyo = DBCommon.GetString(reader, "TYO");
+
+                            list.Add(table);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+    }
+}
---
+++ WebApplication4/Common/DBCommon.cs
@@ -0,0 +1,44 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Common
+{
+    public class DBCommon : IDisposable
+    {
+        public OracleConnection Con { get; }
+
+        public DBCommon()
+        {
+            string dbstring = "Data Source = (DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=hostname)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=xe))); User Id = scott; Password = tiger";
+            Con = new OracleConnection(dbstring);
+            Con.Open();
+        }
+
+        public void Dispose()
+        {
+            if (Con != null)
+            {
+                if (Con.State == System.Data.ConnectionState.Open)
+                {
+                    Con.Close();
+                }
+            }
+        }
+
+        public static string GetString(OracleDataReader read, string target)
+        {
+            if (read.GetValue(read.GetOrdinal(target)).ToString() == "")
+            {
+                return "";
+            }
+            else
+            {
+                return read.GetString(read.GetOrdinal(target));
+            }
+        }
+
+    }
+}
